d4100666b77b5387558b0794a5cae4d61a0e0152
PeymaanEsi <79084586+PeymaanEsi@users.noreply.github.com>
2023-05-31 18:30:07 +0330

    Some Changes

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 size_n = 8
 
+samples = 100
+
 max_fitness = size_n * (size_n - 1) // 2
+
+cutshare = 0.5
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import config, generate, fitnessp, sort
 
-cs = generate.generate(5)
+cs = generate.generate(config.samples)
 
 cs.append([2, 5, 7, 1, 3, 0, 6, 4])
 cs.append([1, 0, 2, 3, 7, 5, 6, 4])
