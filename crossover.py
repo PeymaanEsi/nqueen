87701f8210f0d8b40972d2fb39622a1b6089db3f
unknown <karimi.mahdieh2002@gmail.com>
2023-06-02 13:32:38 +0330

    chert 5

--- crossover.py
+++ crossover.py
@@ -1,17 +1,20 @@
 import random
+from config import size_n
 
 def crossover(chroms):
     mask = [random.randrange(2) for i in range(8)]
     print(mask)
     newchroms = []
-       if mask==1: 
+    if mask==1: 
         for i in range(0,chroms,2):
             for j in range(0,size_n):
-                newchroms[i]=chroms[i], newchroms[i+1]=chroms[i+1]
-        elif mask==0: 
+                newchroms[i].append(chroms[i])
+                newchroms[i+1].append(chroms[i+1])
+    elif mask==0: 
         for i in range(0,chroms,2):
             for j in range(0,size_n):
-                newchroms[i]=chroms[i+1],newchroms[i+1]=chroms[i]
+                newchroms[i].append(chroms[i+1])
+                newchroms[i+1].apppend(chroms[i])
                 
     # /   ///////
     
--- mutation.py
+++ mutation.py
@@ -1,10 +1,11 @@
-   if mask==1: 
-        for i in range(0,chroms,2):
-            for j in range(0,size_n):
-                newchroms[i]=chroms[i], newchroms[i+1]=chroms[i+1]
-    elif mask==0: 
-        for i in range(0,chroms,2):
-            for j in range(0,size_n):
-                newchroms[i]=chroms[i+1],newchroms[i+1]=chroms[i]
-                
-    # /   ///////
+from config import size_n
+import random 
+import numpy as np 
+def mutation(chroms):
+    for chrom in chroms: 
+        for i in chrom:
+            low=np.random.randint(0,size_n-1)
+            up=np.random.randint(1,size_n-1)
+            scramble=np.random.choice(np.arange(low,up+1))
+    return scramble
+            
